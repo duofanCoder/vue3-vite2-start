9fac543c5daa6addf960058d2d39b51685c47ee8
duofan <2441051071@qq.com>
2022-06-21 19:06:43 +0800

    duofan

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,11 @@
 <template>
-<n-button>Weclome</n-button> 
+  <div class="flex">
+    <div>
+      <a href="http://www.duofan.top">
+        <n-button>Weclome</n-button>
+      </a>
+    </div>
+  </div>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <style scoped></style>
